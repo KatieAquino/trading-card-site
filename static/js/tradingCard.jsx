0d3d71df36f72da0f3a29ed87ec9a83c5b0a01de
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-02-03 06:07:42 +0000

    Adds new trading cards to page

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -4,15 +4,57 @@ const tradingCardData = [
     name: 'Balloonicorn',
     skill: 'video games',
     imgUrl: '/static/img/balloonicorn.jpg'
-  }
-]
+  },
 
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'Morphy',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpg'
+  },
+
+  {
+    name: 'Merial',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.jpg'
+  }
+];
 function TradingCard(props) {
   return (
     <div className="card">
       <h2>Name: {props.name}</h2>
       <img src={props.imgUrl} />
-      <h2>Skill: </h2>
+      <h2>Skill: {props.skill}</h2>
     </div>
   );
 }
@@ -48,4 +90,26 @@ ReactDOM.render(
     />
   ),
   document.querySelector('#llambda')
+);
+
+ReactDOM.render(
+  (
+    <TradingCard
+      name="Merial"
+      skill="Swimming Gracefully"
+      imgUrl="static/img/merge.jpg"
+    />
+  ),
+  document.querySelector('#merial')
+);
+
+ReactDOM.render(
+  (
+    <TradingCard
+      name="Morphy"
+      skill="Polymorphism"
+      imgUrl="static/img/polymorphism.jpg"
+    />
+  ),
+  document.querySelector('#morphy')
 );
